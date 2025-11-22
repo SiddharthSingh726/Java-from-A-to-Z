9aedb8369ce9d15ddbca05c357d97b6bb594c40a
Siddharth Singh <siddharthsingh21122002@gmail.com>
2025-11-22 15:47:49 +0530

    added Naming_Convention_in_Java file

---
+++ Naming_Conventions_And_Data_Types/Naming_Conventions_in_Java.java
@@ -0,0 +1,42 @@
+/* Naming conventions should follow a particular or a specific rules.
+* and those rules are :-
+ */
+
+/* 1. Naming a packages ----> Names of packages in Java are written in small letters as :-
+  * java.awt
+  * java.io
+  * javax.swing
+ */
+
+//-------------------------------------------------------------------------------------------------------
+
+/* 2. Naming a class ----> Names of class names and interface names start with a capital letter as :-
+ * String
+ * DataInputStream
+ * ActionListener
+ */
+
+//-------------------------------------------------------------------------------------------------------
+
+/* 3. Method and Variables ----> The first word of a method name is in small letters; then from second word onwards,
+ * each new word starts with a capital letter as :-
+ * println()
+ * readLine()
+ * getNumberInstance()
+ */
+
+//-------------------------------------------------------------------------------------------------------
+
+/* 4. Constant ----> Constants should be written by using all capital letter as :-
+ * PI
+ * MAX_VALUE
+ * font.BOLD -----> here BOLD is constant in Font class.
+ */
+
+//-------------------------------------------------------------------------------------------------------
+
+/* 5. Keywords ----> All Keywords should be written in a small letter as :-
+ * public
+ * static
+ * void
+ */
