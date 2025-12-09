c4f390c73adbf4d1beb471037dd01ae23a31076f
Siddharth Singh <siddharthsingh21122002@gmail.com>
2025-12-09 11:20:26 +0530

    Resume

---
+++ Introduction_to_OOPS/InheritanceMethodOverriding.java
@@ -0,0 +1,21 @@
+// We going to learn about Inheritance Overriding.
+package Introduction_to_OOPS;
+
+class Animal4{
+    String name;
+    void talk(){
+        System.out.println("I am a talk");
+    }
+}
+class Parrot extends Animal4{
+    @Override
+    void talk() {
+            System.out.println("Parrot can Speak");
+    }
+}
+public class InheritanceMethodOverriding {
+    public static void main(String [] args){
+        Parrot p = new Parrot ();
+        p.talk();
+    }
+}
