f152fa91fc51aa8214ec5238e538fbe5bc6e9a69
SiddharthSingh <SiddharthSingh21122002@gmail.com>
2026-01-09 20:25:56 +0530

    Done

---
+++ Classes_and_objects/Demo_Code_1.java
@@ -0,0 +1,26 @@
+package Classes_and_objects;
+
+class Person{
+    //properties - instance variable
+    String name;
+    int age;
+Person(String name,int age){
+    this.name = name;
+    this.age = age;
+}
+    //action - methods
+    void talk(){
+        System.out.println("Hello I am "+name);
+        System.out.println("My age is "+age);
+    }
+}
+public class Demo_Code_1 {
+    public static void main(String[] args) {
+        Person p1 = new Person("Sid",23);
+        System.out.println(p1.name);
+        p1.name = "Siddharth";
+        System.out.println(p1.name);
+        p1.age = 23;
+        p1.talk();
+    }
+}
