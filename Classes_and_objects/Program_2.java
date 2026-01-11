db07141cae02c48954166f71b15ad88dff74fb9e
SiddharthSingh <SiddharthSingh21122002@gmail.com>
2026-01-11 05:38:55 +0530

    Updated

---
+++ Classes_and_objects/Program_2.java
@@ -0,0 +1,19 @@
+//Let us rewrite Program 1, where we want to ca the talk() method.
+package Classes_and_objects;
+
+class Person3{
+    String name;
+    int age;
+
+    void talk(){
+        System.out.println(name + " is talking and his age is "+age);
+    }
+}
+public class Program_2 {
+    public static void main(String [] args){
+        Person3 p1 = new Person3();
+        p1.name = "Raju";
+        p1.age = 20;
+        p1.talk();
+    }
+}
