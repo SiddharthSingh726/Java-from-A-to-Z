25a24ff108fb612b3374a383bb9fce4ff7ca47ef
SiddharthSingh <SiddharthSingh21122002@gmail.com>
2026-01-17 10:42:29 +0530

    Updated

--- Arrays/Program_3.java
+++ Arrays/Program_3.java
@@ -4,41 +4,30 @@ import java.util.Scanner;
 
 public class Program_3 {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter the value of n: ");
+        System.out.print("Enter the value of n  : ");
         int n = sc.nextInt();
-
         int[] arr = new int[n];
-
-        System.out.print("Enter the random numbers: ");
+        System.out.print("Enter the random numbers :- ");
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-
-        boolean swapped;
-
-        for (int i = 0; i < arr.length - 1; i++) {
-            swapped = false;
-
-            for (int j = 0; j < arr.length - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-
-                    swapped = true;
+        boolean Swapped;
+        for (int i = 0; i < n; i++) {
+            Swapped = false;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                    Swapped = true;
                 }
             }
-
-            if (!swapped)
-                break; // Array already sorted
+            if (!Swapped)
+                break;
         }
-
-        System.out.println("The sorted array is: ");
-        for (int i = 0; i < arr.length; i++) {
+        System.out.print("The sorted array is:- ");
+        for (int i = 0; i < n; i++) {
             System.out.print(arr[i] + " ");
         }
     }
