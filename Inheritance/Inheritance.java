dff1a2e57b39eed88fee2ab956185a30dc0246a1
SiddharthSingh <SiddharthSingh21122002@gmail.com>
2026-01-23 15:56:15 +0530

    Updated

--- Inheritance/Inheritance.java
+++ Inheritance/Inheritance.java
@@ -1,4 +1,23 @@
 package Inheritance;
 
+class Animals{
+    String name;
+    int age;
+    void eat(){
+        System.out.println(name+" Animals eat and his age is "+age);
+    }
+}
+class Dog extends Animals{
+    void sleep(){  //here the child class is able to access the name and age because child class have access of both parent and child member access.
+        System.out.println(name+" is the Dog sleep and his age is "+age);
+    }
+}
 public class Inheritance {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.name = "Tommy";
+        d.age = 20;
+        d.eat();
+        d.sleep();
+    }
 }
--- Inheritance/Introduction.java
+++ Inheritance/Introduction.java
@@ -5,4 +5,8 @@
    features of the existing class from where it is derived.
 
    A child class object can access parent class non-private members.
+
+   Child classes can access parent class public and protected members, but not private members.
+
+   Parent constructor is ALWAYS called before child constructor.
  */
