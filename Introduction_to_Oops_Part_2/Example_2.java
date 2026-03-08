fbd07751d901a1d137b674e8d4b7b50258eaf534
Siddharth Singh <Siddharthsingh21122002@gamil.com>
2026-03-08 22:28:56 +0530

    Updated

---
+++ Introduction_to_Oops_Part_2/Example_2.java
@@ -0,0 +1,12 @@
+package Introduction_to_Oops_Part_2;
+
+class Student1 {
+    Student1(){ // this is called Construction
+        System.out.println("Object Created");
+    }
+}
+public class Example_2 {
+    public static void main(String[] args) {
+        Student1 s1 = new Student1();
+    }
+}
--- Introduction_to_Oops_Part_2/Introduction.java
+++ Introduction_to_Oops_Part_2/Introduction.java
@@ -4,4 +4,28 @@
     a.State(properties/data)
     b.Behaviour(actions/methods)
   Eg - Student have State = name and Behaviour = Studying
+
+  2.Creation of Object(How to use a class in Java)
+  Syntax for creating an object in Java.
+     ClassName objectName = new ClassName()
+  Eg - public class main(){
+            public static void main (String [] args) {
+            Car myCar = new Car(); // creating object
+
+            myCar.color = "Red" // setting state
+            myCar.speed = 100;
+
+            myCar.drive(); // calling behaviour
+          }
+       }
+
+   Output - Car is driving
+
+   2.Constructors
+   Ans.A constructor is a special method that runs automatically when an object is created.
+       It is used to initialize object data.
+   Important rules of Construction.
+   a.Constucutor name =  same as class name
+   b.No return type(not even void)
+   c.Runns automatically when object is created.
  */
